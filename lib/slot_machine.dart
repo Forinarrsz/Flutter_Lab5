49288b5bee501ac53cace80309bb1c5c787dcf7d
K1eranza <zubrevaevelina20641@gmail.com>
2026-04-29 15:00:25 +0300

    Add coin protection, disabled button state and reset button

--- lib/slot_machine.dart
+++ lib/slot_machine.dart
@@ -25,6 +25,12 @@ class _SlotMachineState
   var _message = '';
 
   void _spin() {
+    if(_coins <= 0) {
+      setState(() {
+        _message = 'Монеты закончились!';
+      });
+      return;
+    }
     setState(() {
       _slot1 =
           _symbols[_random.nextInt(
@@ -49,7 +55,15 @@ class _SlotMachineState
       }
     });
   }
-
+  void _reset() {
+    setState(() {
+      _coins = 10;
+      _slot1 = 'assets/images/cherry.png';
+      _slot2 = 'assets/images/lemon.png';
+      _slot3 = 'assets/images/seven.png';
+      _message = '';
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -97,7 +111,7 @@ class _SlotMachineState
         ),
         SizedBox(height: 40),
         ElevatedButton(
-          onPressed: _spin,
+          onPressed: _coins > 0 ? _spin : null,
           style: ElevatedButton.styleFrom(
             backgroundColor: Colors.amber,
             padding: EdgeInsets.symmetric(
@@ -114,7 +128,20 @@ class _SlotMachineState
             ),
           ),
         ),
-      ],
+          SizedBox(height: 12),
+          TextButton(
+            onPressed: _reset,
+            child: Text(
+              'Начать заново',
+              style: TextStyle(
+                color: Colors.white70,
+                fontSize: 16
+              ),
+            ),
+          ),
+        
+      ]     
     );
+        
   }
 }
